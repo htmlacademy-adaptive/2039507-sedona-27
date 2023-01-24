d6bb270ab8146d9a482ca911101035f608b98e07
igor <igorbolduev1989@gmail.com>
2023-01-24 15:40:29 +0700

    добавляет pixel perfect modal

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ const copyImages = () => {
 
 //Webp
 
-export const createWebp = () => {
+const createWebp = () => {
   return gulp
     .src("source/img/**/*.{jpg,png}", "!source/img/favicons")
     .pipe(squoosh({ webp: {} }))
